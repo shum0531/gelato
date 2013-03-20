dddd6638feef33a0349b342f9d59a7f132b9589e
Sergey Romanenko <awilum@msn.com>
2013-03-20 09:37:53 +0200

    Html Class: obfuscate() method added.

--- Html/Html.php
+++ Html/Html.php
@@ -279,7 +279,41 @@ class Html
         $attributes['alt'] = (isset($attributes['alt'])) ? $attributes['alt'] : pathinfo($file, PATHINFO_FILENAME);
 
         return '<img'.Html::attributes($attributes).' />';
-    }
+    }    
+    
+    /**
+	 * Obfuscate a string to prevent spam-bots from sniffing it.
+	 *
+     * This method obfuscate the value, randomly convert each
+     * letter to its entity or hexadecimal representation, keeping a
+     * bot from sniffing the randomly obfuscated letters.
+     *
+     *    <code>
+     *  	echo Html::obfuscate('gelato@monstra.org');
+     * 	</code>
+     *
+	 * @param  string  $value
+	 * @return string
+	 */
+	public static function obfuscate($value)
+	{
+		$safe = '';
+
+		foreach (str_split($value) as $letter) {
+			switch (rand(1, 3)) {
+				case 1:
+					$safe .= '&#'.ord($letter).';';
+				break;
+				case 2:
+					$safe .= '&#x'.dechex(ord($letter)).';';
+				break;
+				case 3:
+					$safe .= $letter;
+			}
+		}
+
+		return $safe;
+	}
 
     /**
      * Convert html to plain text
